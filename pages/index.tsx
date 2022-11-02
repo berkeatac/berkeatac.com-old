63551157a28245dc7df0e505906c22c588a3d2a9
Berke Atac <berke.atac@gmail.com>
2022-11-02 21:16:36 +0100

    fix: type issues

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,60 +1,63 @@
 import type { NextPage } from "next";
 import Link from "next/link";
-import { useEffect, useRef } from "react";
+import { ReactHTMLElement, useEffect, useRef } from "react";
 
 const Home: NextPage = () => {
   const canvasRef = useRef(null);
+  var step = 0;
+  function horizontalSine(ctx: CanvasRenderingContext2D, xOffset: number) {
+    var width = ctx?.canvas.width;
+    var height = ctx?.canvas.height;
 
-  useEffect(() => {
-    var step = 0;
-    var horizontalCanvas = document.getElementById("sine-horizontal");
-    var contextHorizontal = horizontalCanvas.getContext("2d");
+    ctx?.beginPath();
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "black";
 
-    function horizontalSine(ctx, xOffset) {
-      var width = ctx.canvas.width;
-      var height = ctx.canvas.height;
-
-      ctx.beginPath();
-      ctx.lineWidth = 2;
-      ctx.strokeStyle = "black";
-
-      var x = 0;
-      var y = 0;
-      var amplitude = 40;
-      var frequency = 40;
-      while (x < width) {
-        y = height / 2 + amplitude * Math.sin((x + xOffset) / frequency);
-        ctx.lineTo(x, y);
-        x++;
-      }
-      ctx.stroke();
-      ctx.save();
-
-      ctx.stroke();
-      ctx.restore();
+    var x = 0;
+    var y = 0;
+    var amplitude = 40;
+    var frequency = 40;
+    while (x < width) {
+      y = height / 2 + amplitude * Math.sin((x + xOffset) / frequency);
+      ctx.lineTo(x, y);
+      x++;
     }
+    ctx.stroke();
+    ctx.save();
 
-    function draw() {
-      contextHorizontal.clearRect(0, 0, 1500, 1500);
+    ctx.stroke();
+    ctx.restore();
+  }
 
-      horizontalSine(contextHorizontal, step, 50);
-      contextHorizontal.restore();
+  function draw(contextHorizontal: CanvasRenderingContext2D | null) {
+    if (contextHorizontal) {
+      contextHorizontal?.clearRect(0, 0, 1500, 1500);
+
+      horizontalSine(contextHorizontal, step);
+      contextHorizontal?.restore();
 
       step += 1;
-      window.requestAnimationFrame(draw);
+      window.requestAnimationFrame(() => draw(contextHorizontal));
     }
+  }
 
-    function init() {
-      draw();
+  function init(contextHorizontal: CanvasRenderingContext2D | null) {
+    draw(contextHorizontal);
+  }
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      var horizontalCanvas: HTMLCanvasElement = canvasRef.current;
+      var contextHorizontal = horizontalCanvas?.getContext("2d");
+
+      init(contextHorizontal);
+
+      return () => {
+        // cleanup
+        contextHorizontal?.clearRect(0, 0, 1500, 1500);
+        canvasRef.current = null;
+      };
     }
-
-    init();
-
-    return () => {
-      // cleanup
-      contextHorizontal.clearRect(0, 0, 1500, 1500);
-      canvasRef.current = null;
-    };
   }, [canvasRef.current]);
 
   return (

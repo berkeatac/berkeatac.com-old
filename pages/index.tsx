6f666a3307e8200ac9d4893b3af690f4a8f6b079
Berke Atac <berke.atac@gmail.com>
2022-11-02 18:12:35 +0100

    chore: add solsten link to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
     <>
       <h2 className="text-6xl leading-loose">I'm Berke</h2>
-      <h3 className="text-4xl leading-relaxed">Frontend Developer</h3>
+      <h3 className="text-4xl leading-relaxed">
+        Frontend Developer @
+        <Link href={"https://solsten.io"} className="underline" target="_blank">
+          Solsten
+        </Link>
+      </h3>
       <h3 className="text-4xl leading-relaxed">Berlin</h3>
       <h3 className="text-4xl leading-relaxed">React</h3>
     </>
